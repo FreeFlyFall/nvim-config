a34596f2c2ebe75a9f96d5c19026eacb662c17fb
x <x@x.com>
2022-12-19 17:46:57 -0500

    update to fix file history

--- lua/set.lua
+++ lua/set.lua
@@ -10,9 +10,10 @@ vim.opt.smartindent = true
 
 vim.opt.wrap = true
 
+-- Make a change comment here?
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME" .. "/.vim/undodir")
+-- vim.opt.undodir = os.getenv("HOME" .. "/.vim/undodir")
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
